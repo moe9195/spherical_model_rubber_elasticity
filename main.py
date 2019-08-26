3fd1c5fc967a1ceebda8caff78d72b90c482dd0f
moe9195 <49087908+moe9195@users.noreply.github.com>
2019-08-26 06:23:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import RubberElasticity as rs
 import numpy as np
 import matplotlib.pyplot as plt
-
 np.seterr(divide='ignore', invalid='ignore');
 
-h, defType, distType, n, G, N, tolerance, std = 0.02, 1, 1, 10, 0.135, 10000, 0.9, 5
+'''
+testing the model by plotting the stress-strain relation for an elastomer undergoing
+uniaxial cyclic loading to 200%, 250%, and 300%.
+'''
 
+h, defType, distType, n, G, N, tolerance, std = 0.02, 1, 1, 10, 0.135, 10000, 0.9, 5
 lam_max = np.linspace(2, 3, 3)
 fig = plt.figure()
-for i in lam_max:
-    lam0, F, Fnew = rs.stress_strain_relation(n, N, G, h, std, tolerance, i, defType, distType)
+for x in lam_max:
+    lam0, F, Fnew = rs.stress_strain_relation(n, N, G, h, std, tolerance, x, defType, distType)
     rs.plot_relation(lam0, F, Fnew, fig)
 plt.show()
